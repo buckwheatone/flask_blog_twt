238a7c443331359898bc0fd2147ec08bb8200513
BuckwheatOne <33817412+buckwheatone@users.noreply.github.com>
2021-09-24 22:25:22 -0500

    fix JS on like, delete

--- website/static/index.js
+++ website/static/index.js
@@ -1,15 +1,20 @@
 function like(postId) {
   const likeCount = document.getElementById(`likes-count-${postId}`);
   const likeButton = document.getElementById(`like-button-${postId}`);
+  // const likeIcon = document.getElementsByClassName('like')
+
 
   fetch(`/like-post/${postId}`, { method: "POST" })
     .then((res) => res.json())
     .then((data) => {
       likeCount.innerHTML = data["likes"];
       if (data["liked"] === true) {
-        likeButton.className = "fas fa-thumbs-up";
+        likeButton.innerHTML = "favorite";
+        // likeIcon.innerHTML = 'favorite'
+
       } else {
-        likeButton.className = "far fa-thumbs-up";
+        likeButton.innerHTML = "favorite_outline";
+        // likeIcon.innerHTML = 'favorite_outline'
       }
     })
     .catch((e) => alert("Could not like post."));
