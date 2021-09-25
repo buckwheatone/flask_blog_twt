dabbd4ebfb8a0d226d24e1ee74eadf1bde97d75c
BuckwheatOne <33817412+buckwheatone@users.noreply.github.com>
2021-09-25 09:54:33 -0500

    fix delete error

--- website/static/index.js
+++ website/static/index.js
@@ -1,7 +1,6 @@
 function like(postId) {
   const likeCount = document.getElementById(`likes-count-${postId}`);
   const likeButton = document.getElementById(`like-button-${postId}`);
-  // const likeIcon = document.getElementsByClassName('like')
 
 
   fetch(`/like-post/${postId}`, { method: "POST" })
@@ -10,11 +9,8 @@ function like(postId) {
       likeCount.innerHTML = data["likes"];
       if (data["liked"] === true) {
         likeButton.innerHTML = "favorite";
-        // likeIcon.innerHTML = 'favorite'
-
       } else {
         likeButton.innerHTML = "favorite_outline";
-        // likeIcon.innerHTML = 'favorite_outline'
       }
     })
     .catch((e) => alert("Could not like post."));
