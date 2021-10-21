e30c4c6c3d9195851464ac2b60ea07a44b5f66d8
BuckwheatOne <33817412+buckwheatone@users.noreply.github.com>
2021-10-21 15:37:59 -0500

    update config, structure, add draft of tests

--- website/__init__.py
+++ website/__init__.py
@@ -2,18 +2,19 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail 
+from website.config import Config
 from os import path
 
 db = SQLAlchemy()
-DB_NAME = "twtblogdb.db"
 login_manager = LoginManager()
 login_manager.login_view = "auth.login"
 mail = Mail()
 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
+    app.config.from_object(config_class)
     app.config['SECRET_KEY'] = "helloworld"
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    
     
     db.init_app(app)
     login_manager.init_app(app)
@@ -33,5 +34,5 @@ def create_app():
 
 
 def create_database(app):
-    if not path.exists("website/" + DB_NAME):
+    if not path.exists("website/" + Config.DB_NAME):
         db.create_all(app=app)
